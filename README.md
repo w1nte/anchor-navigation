82abaf68b430172b4fb05063dd7442faae6144a2
winte <miwel33@gmail.com>
2018-08-16 12:21:32 +0200

    Initial Commit #5

--- README.md
+++ README.md
@@ -1,33 +1,58 @@
 # Anchor Navigation
 
-JS Anchor Navigation Library that automatically updates the navigation current class. [See Example](https://w1nte.github.io/anchor_navigation/)
+JS Anchor Navigation Library that automatically updates the navigation current class if scrolling. [See Example](https://w1nte.github.io/anchor_navigation/)
 
 ## Features
+* Easy to integrate!
 * Automatically updates navigation's current item.
-* Uses modern web technique for more performance
-* Polyfill's integrated!
 
 ## Usage
-#### with jQuery
-`````javascript
+##### with jQuery
+```javascript
 $(document).ready(function() {
 
     $('#navigation li').anchorNavigation({
         "navigation_current_class": "current",
         "home_anchor": "home",
-        "onclick_callback": function(e) {} // click on navigation links
+        "navigation_onclick_callback": function(entry, e) {
+            // smooth scrolling, requires jquery
+            $('html, body').animate({
+                scrollTop: entry.key === 'home' ? 0 : (entry.heading_container.offsetTop)
+            }, 'slow');
+            e.preventDefault();
+        }
     });
 
 });
-`````
+```
 
-#### without jQuery
-`````javascript
-var anchor_nav = new AnchorNavigation('#navigation li', {
-        "navigation_current_class": "current",
-        "home_anchor": "home",
-        "onclick_callback": function(e) {} // click on navigation links
-    });
-`````
+##### without jQuery
+```javascript
+window.addEventListener("load", function(event) {
+    var anchor_nav = new AnchorNavigation('#navigation li', {
+            "navigation_current_class": "current",
+            "home_anchor": "home",
+            "navigation_onclick_callback": function(entry, e) { }
+        });
+}, false);
+```
 
-If you want smooth scrolling, see index.html (requires jquery)
+### Available Arguments
+```javascript
+{
+    "navigation_current_class":     ["current"],                    // current page class
+    "heading_container":            "div, section, article",        // container which separates the content
+    "home_anchor":                  "home",                         // virtual anchor of the original home list element
+    "threshold":                    0.5,
+    "timeout":                      250,                            // scrolling debounce timeout
+    "navigation_onclick_callback":  function (entry, e) { },
+    "navigation_onupdate_callback": function (entry) { }
+}
+```
+
+## Supported Browsers
+* Edge >= 17
+* Chrome >= 68
+* Firefox >= 61
+
+The Library doesn't work in IE without polyfills!
--- anchor-navigation.js
+++ anchor-navigation.js
@@ -1,91 +1,70 @@
-"use strict";
-
-const INTERSECTION_OBSERVER = ("IntersectionObserver" in window);
-//const INTERSECTION_OBSERVER = false; // debug
-
 
 class AnchorNavigation {
-    constructor (navigation_entries, config) {
+    constructor (navigation_list_elements, config) {
         let $this = this;
 
         this.defaultConfig = {
-            "navigation_entries": "#navigation li",
-            "navigation_current_class": "current",
+            "navigation_list_elements": "#navigation li",
+            "navigation_current_class": ["current"],
+            "heading_container": "div, section, article",
             "home_anchor": "home",
-            "onclick_callback": function (e) {
-            }
+            "threshold": 0.5,
+            "timeout": 250, // debounce timeout
+            "navigation_onclick_callback": function (entry, e) { },
+            "navigation_onupdate_callback": function (entry) { }
         };
-        this.config = this.extend({}, this.defaultConfig, {"navigation_entries": navigation_entries}, config);
+        this.config = AnchorNavigation.extend({}, this.defaultConfig, {"navigation_list_elements": navigation_list_elements}, config);
 
-        let nav_entries = this.find_nav_entries(this.config["navigation_entries"]);
+        this.nav_entries = new NavEntries();
 
-        let observer = this.register_observer(function(entry) {
-            nav_entries.set_current_page_class(entry.id, $this.config["navigation_current_class"]);
-        });
+        this.update_nav_entries();
+    }
 
-        if (!observer) { // fallback
-            let si = 0, scroll_precision = 5;
-            window.addEventListener("scroll", function() {
-                si++;
-                if (si > scroll_precision) {
+    update_nav_entries () {
+        let $this = this;
 
-                    nav_entries.entries.forEach(entry => {
-                        if (entry.key) {
-                            let anchorDest = document.querySelector("#" + entry.key);
-                            if (anchorDest) {
-                                let domRect = anchorDest.getBoundingClientRect();
-                                if (domRect.top < window.innerHeight && domRect.top > 0)
-                                    nav_entries.set_current_page_class(anchorDest.id, $this.config["navigation_current_class"]);
-                            }
-                        }
-                    });
+        this.nav_entries = this.find_nav_entries(this.config.navigation_list_elements);
 
-                    si = 0;
-                }
+        let is_scrolling;
+        window.addEventListener("scroll", function() {
+            window.clearTimeout(is_scrolling);
+            is_scrolling = setTimeout(function() {
 
-            })
-        }
+                $this.update($this.nav_entries);
 
-        nav_entries.entries.forEach(entry => {
-            if (entry.key) {
-                let anchorDest = document.querySelector("#" + entry.key);
+            }, $this.config.timeout);
+        }, false);
 
-                // observe elements
-                if (anchorDest && observer)
-                    observer.observe(anchorDest);
-
+        this.nav_entries.entries.forEach(entry => {
+            if (entry.anchor_element) {
                 // add entries event listeners
                 entry.anchor_element.addEventListener("click", function(e) {
-                    $this.config["onclick_callback"](e);
+                    $this.config.navigation_onclick_callback(entry, e);
+                    $this.nav_entries.set_current_page_class(entry.key, $this.config.navigation_current_class);
                 });
             }
         });
     }
 
-    register_observer (callback) {
-        if (!INTERSECTION_OBSERVER)
-            return false;
+    update (nav_entries) {
+        let padding = window.innerHeight * this.config.threshold;
 
-        let observer;
+        nav_entries.entries.forEach(entry => {
 
-        let options = {
-            root: null,
-            rootMargin: "0px",
-            threshold: 1
-        };
+            if (entry.key) {
+                let domRect = entry.heading_container.getBoundingClientRect();
 
-        let cb = function(entries, observer) {
-
-            entries.forEach(entry => {
-                if (entry.intersectionRatio >= 1) {
-                    callback(entry.target);
+                if (typeof domRect.height !== "undefined" && domRect.height <= padding) {
+                    padding *= domRect.height / padding - 0.1; // adjust the threshold
                 }
-            });
-        };
 
-        observer = new IntersectionObserver(cb, options);
+                if (domRect.top - padding <= 0 && domRect.bottom - padding >= 0) {
+                    if (nav_entries.set_current_page_class(entry.key, this.config.navigation_current_class))
+                        this.config.navigation_onupdate_callback(entry);
+                }
+            }
 
-        return observer;
+        });
     }
 
     find_nav_entries (li_elements) {
@@ -94,27 +73,48 @@ class AnchorNavigation {
 
         nav_li_elements.forEach(li_element => {
             let anchor_element = li_element;
-            if (li_element.tagName !== "A")
+            if (anchor_element.tagName !== "A") {
                 anchor_element = li_element.querySelector("a");
-
-            let anchor = anchor_element.href.split("#")[1];
-            if (li_element.classList.contains(this.config["navigation_current_class"])) {
-                anchor = this.config["home_anchor"];
-                anchor_element.href += "#" + this.config["home_anchor"];
             }
-            if (anchor)
-                nav_entries.add(anchor, li_element, anchor_element);
+
+            let key = anchor_element.href.split("#")[1];
+
+            if (li_element.classList.contains(this.config.navigation_current_class)) {
+                key = this.config.home_anchor;
+                anchor_element.href += "#" + this.config.home_anchor;
+            }
+
+            let heading_element = document.querySelector("#" + key);
+            let heading_container = AnchorNavigation.querySelectorParent(heading_element, this.config.heading_container);
+
+            if (!heading_container) {
+                heading_container = heading_element;
+            }
+
+            if (key && heading_element) {
+                nav_entries.add(key, li_element, anchor_element, heading_element, heading_container);
+            }
         });
 
         return nav_entries;
-    };
+    }
 
-    extend (...args) {
+    static querySelectorParent (elm, selector) {
+        while (elm) {
+            elm = elm.parentElement;
+            if (elm && elm.matches(selector)) {
+                return elm;
+            }
+        }
+        return false;
+    }
+
+    static extend (...args) {
         for(let i=1; i<args.length; i++)
             for(let key in args[i])
                 if(args[i].hasOwnProperty(key)) {
                     if (AnchorNavigation.is_dictionary(args[i][key]))
-                        args[0][key] = this.extend(args[0][key], args[i][key]);
+                        args[0][key] = AnchorNavigation.extend(args[0][key], args[i][key]);
                     else
                         args[0][key] = args[i][key];
                 }
@@ -122,13 +122,23 @@ class AnchorNavigation {
     }
 
     static is_dictionary (variable) {
-        return typeof variable === "object" && variable!==null && !(variable instanceof Array) && !(variable instanceof Date)
+        return typeof variable === "object" && variable!==null && !(variable instanceof Array) && !(variable instanceof Date);
+    }
+
+    static buildThresholdList (steps=10) {
+        let thresholds = [.0];
+        for (let i=1.0; i<=steps; i++) {
+            let ratio = i/steps;
+            thresholds.push(ratio);
+        }
+        return thresholds;
     }
 }
 
 class NavEntries {
     constructor (options) {
         this.entries = [];
+        this.current_key = undefined;
     }
 
     push (nav_entry) {
@@ -136,28 +146,48 @@ class NavEntries {
             this.entries.push(nav_entry);
     }
 
-    add (key, list_element, anchor_element) {
-        let nav_entry = new NavEntry(key, list_element, anchor_element);
+    add (key, list_element, anchor_element, heading_element, heading_container) {
+        let nav_entry = new NavEntry(key, list_element, anchor_element, heading_element, heading_container);
         this.push(nav_entry);
     }
 
     find (key) {
-        return this.entries.find(function(elm) {
+        return this.entries.find(function (elm) {
             return elm.key === key;
         });
     }
 
     set_current_page_class (key, class_name) {
+        if (this.current_key === key) {
+            return false;
+        }
+
+        if (!Array.isArray(class_name)) {
+            class_name = [class_name];
+        }
+
         this.entries.map(entry => entry.list_element.classList.remove(class_name));
-        this.find(key).list_element.classList.add(class_name);
+        let entry = this.find(key);
+
+        if (!entry) {
+            return false;
+        }
+
+        entry.list_element.classList.add(class_name);
+
+        this.current_key = key;
+
+        return true;
     }
 }
 
 class NavEntry {
-    constructor (key, list_element, anchor_element) {
+    constructor (key, list_element, anchor_element, heading_element, heading_container) {
         this.key = key; // anchor without hash for identification
         this.list_element = list_element; // the list element with the current page class e.g <li>
         this.anchor_element = anchor_element; // the anchor element with the url e.g <a>
+        this.heading_element = heading_element; // anchor destination
+        this.heading_container = heading_container; // container that contains the heading element
     }
 }
 
@@ -167,7 +197,7 @@ if (typeof jQuery !== 'undefined')
 (function($) {
 
     $.fn.anchorNavigation = function(options={}) {
-        let anchor_nav = new AnchorNavigation(this.selector, options);
+        let anchor_nav = new AnchorNavigation(this.selector ? this.selector : this[0], options);
         return this;
     };
 
